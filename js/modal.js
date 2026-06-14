c0f667f45bfa67f48d34c01ec1a14c03af71894f
Abay <moffanggevalk3@gmail.com>
2026-06-14 22:04:01 +0500

    small refactor of modal and index

--- js/modal.js
+++ js/modal.js
@@ -1,26 +1,29 @@
-function openModal() {
-    document.getElementById('modal').style.display = 'flex';
-    document.body.classList.add('no-scroll');
+const modal = document.getElementById('modal')
+const openBtn = document.querySelector('.vacancy-form-button')
+
+const openModal = () => {
+    modal.style.display = 'flex'
+    document.body.classList.add('no-scroll')
 }
 
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-    document.body.classList.remove('no-scroll');
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.classList.remove('no-scroll')
 }
-document.getElementById('modal').addEventListener('click', function(e) {
-    if (e.target === this) {
-      closeModal();
-    }
-});
+
+openBtn.addEventListener('click', openModal)
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) closeModal()
+})
 
 window.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
+    const urlParams = new URLSearchParams(window.location.search)
     if (urlParams.get('success') === '1') {
-        const popup = document.getElementById('form-success-popup');
+        const popup = document.getElementById('form-success-popup')
         if (popup) {
-            popup.style.display = 'block';
-
-            window.history.replaceState({}, document.title, window.location.pathname);
+            popup.style.display = 'block'
+            window.history.replaceState({}, document.title, window.location.pathname)
         }
     }
-});
+}) 
